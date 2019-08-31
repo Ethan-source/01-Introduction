78a59c7a95b6ceed81ccd1cd44f602a6b668a056
ethan lam <ethan@ethans-MacBook-Pro-3.local>
2019-08-31 09:46:38 -0400

    Completes Introduction assignment

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Ethan Yee, I am a sophomore Digital & Interactive Media major from Chicago, IL.')
+
+print('My favorite video game is Super Smash Bros Ultimate for the Nintendo switch. The task of the game seems so simple, knock your friends off the platform before they knock you off. Seems fiarly simple, yet there is so much depth to it that allows a really skilled player to showcase their talents.')
+
+print('Prior to this class, I have absolutely no coding or game design experience. My biggest concern is that the learning curve may be too steep for me to keep up with all of the game design majors')
+
+print('I have been extremely excited to finish furnishing my damn apartment that I share with 4 roomates. It has already been 3 weeks and we still eat on the floor.') 
+
+print('11985119 is my Stockoverflow User Number')
+
+print('https://github.com/Ethan-source is the URL to my GitHub profile')
